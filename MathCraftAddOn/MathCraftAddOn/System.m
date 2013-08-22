2fc2492e43f8b36ed6079038b6460019efb5a75c
MathCraft <mathcraft.org@gmail.com>
2013-08-22 11:26:19 -0500

    + added mcFalseQ

--- MathCraftAddOn/MathCraftAddOn/System.m
+++ MathCraftAddOn/MathCraftAddOn/System.m
@@ -2,4 +2,12 @@
 
 BeginPackage["MathCraftAddOn`System`"]
 
+mcFalseQ::usage = "mcFalseQ[args] is a shorthand of Not[TrueQ[args]]"
+
+Begin["`Private`"]
+
+mcFalseQ[args___] := !TrueQ[args];
+
+
+End[]
 EndPackage[]
