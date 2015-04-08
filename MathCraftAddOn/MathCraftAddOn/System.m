18cd6e510b8c68337b362ecad8ca3421428a56a0
MathCraft <mathcraft.org@gmail.com>
2015-04-07 23:38:52 -0700

    % Renamed mcNestedWith to mcIteratedWith;  Tweaked the implementation so it can be called just like With: mcIteratedWith[{x = x0, y = f(x), z = g(x, y)}, ...].

--- MathCraftAddOn/MathCraftAddOn/System.m
+++ MathCraftAddOn/MathCraftAddOn/System.m
@@ -3,7 +3,11 @@
 BeginPackage["MathCraftAddOn`System`"]
 
 mcFalseQ::usage = "mengFalseQ[expr] yields True if expr is False, and yields False otherwise.  mcFalseQ[args] is NOT equivalent to Not[TrueQ[args]] for all args."
-mcNestedWith::usage = "mcNestedWith[{x=x0, y=y0, ...}, expr] is like mcNestedWith[{x=x0, y=y0, ...}, expr] but allows y0 to be a function of x0"
+
+mcIteratedWith::usage = "mcIteratedWith[{x=x0, y=y0, ...}, expr] is like With[{x=x0, y=y0, ...}, expr] but allows y0 to be a function of x."
+
+mcLet::usage = "mcLet[{x = x0, y = y0, ...}, expr] is similar as mcLet[{x = x0, y = y0, ...}, expr] but XXX."
+
 Begin["`Private`"]
 
 (*markdown!
@@ -31,14 +35,56 @@ mcFalseQ[_] = False;
 
 *)
 
-SetAttributes[mcNestedWith, HoldAll]
+SetAttributes[mcIteratedWith, HoldAll]
 
-mcNestedWith[expr_] := Activate[
+mcIteratedWith[args_, expr__] := Activate[
     ReplaceRepeated[
-        Inactivate[expr],
-        Inactive[With][{first_, rest__}, body_] :> Inactive[With][{first}, Inactive[With][{rest}, body]]
+        Inactivate[With[args, expr]],
+        Inactive[With][{first_, rest__}, body_] :> Inactive[With][
+            {first},
+            Inactive[With][{rest}, body]
+        ]
     ]
 ]
+
+
+
+(*markdown!
+
+
+## References
+
+* http://www.wolfram.com/mathematica/new-in-10/inactive-objects/transform-code.html
+* http://mathematica.stackexchange.com/questions/10432/how-to-avoid-nested-with
+* http://www.wolfram.com/mathematica/new-in-10/inactive-objects/study-maxwells-equations.html
+
+*)
+
+ClearAll[mcLet];
+
+SetAttributes[mcLet, HoldAll];
+
+SyntaxInformation[mcLet] = {
+    "ArgumentsPattern" -> {_, _},
+    "LocalVariables" -> {"Solve", {1, Infinity}}
+};
+
+mcLet /: Verbatim[SetDelayed][lhs_, rhs : HoldPattern[mcLet[{__}, _]]] :=
+    Block[
+        {With},
+        Attributes[With] = {HoldAll};
+        lhs := Evaluate[rhs]
+    ];
+
+mcLet[{}, expr_] := expr;
+
+mcLet[{head_}, expr_] := With[{head}, expr];
+
+mcLet[{head_, tail__}, expr_] := Block[
+    {With},
+    Attributes[With] = {HoldAll};
+    With[{head}, Evaluate[mcLet[{tail}, expr]]]
+];
 
 End[]
 EndPackage[]
